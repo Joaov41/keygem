3f01d933ce0cb90ef24b82690a92a3d38b96a284
Joaov41 <52511571+Joaov41@users.noreply.github.com>
2025-02-06 18:31:28 +0000

    Add files via upload

---
+++ keygemTests/keygemTests.swift
@@ -0,0 +1,17 @@
+//
+//  keygemTests.swift
+//  keygemTests
+//
+//  Created by john val on 1/31/25.
+//
+
+import Testing
+@testable import keygem
+
+struct keygemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
